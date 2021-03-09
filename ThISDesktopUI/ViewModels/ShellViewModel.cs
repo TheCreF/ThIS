a6997af7b9b5d683637f1e9802a2f38f28e546bb
Francesco Crescimanno <fcrescimanno@outlook.com>
2021-03-09 23:56:04 +0100

    Aggiunta la LoginViewModel alla ShellViewModel
    La LoginView, attraver la Dependency Injection viene visualizzata nella ShellView

--- ThISDesktopUI/ViewModels/ShellViewModel.cs
+++ ThISDesktopUI/ViewModels/ShellViewModel.cs
@@ -9,5 +9,12 @@ namespace ThISDesktopUI.ViewModels
 {
     public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+        }
     }
 }
