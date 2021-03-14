a90c9c0923c508de1980019b97d9b5dd33e7e503
Francesco Crescimanno <fcrescimanno@outlook.com>
2021-03-14 18:55:18 +0100

    Aggiunta lettura Dati Utente da DB ThISData
    Impostata e testata la lettura dei dati Utente attraverso laapi/user

---
+++ ThISDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThISDataManager.Library.Internal.DataAccess;
+using ThISDataManager.Library.Models;
+
+namespace ThISDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "ThISDataConnection");
+
+            return output;
+        }
+    }
+}
---
+++ ThISDataManager/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using ThISDataManager.Library.DataAccess;
+using ThISDataManager.Library.Models;
+
+namespace ThISDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
