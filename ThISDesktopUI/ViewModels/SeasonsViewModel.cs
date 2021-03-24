69c3eec072b56e8d7d732358fbc794ad842282bf
Francesco Crescimanno <fcrescimanno@outlook.com>
2021-03-24 01:33:16 +0100

    Season View e ViewModel
    Aggiunta SeasonView e SeasonViewModel, ma non ancor acollegate ai dati. Devono ancora essere create i Metodi per i bottoni "Aggiungi Nuova Stagione", "Salva" e "Annulla"

--- ThISDesktopUI/ViewModels/SeasonsViewModel.cs
+++ ThISDesktopUI/ViewModels/SeasonsViewModel.cs
@@ -1,13 +1,65 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThISDesktopUI.Views;
 
 namespace ThISDesktopUI.ViewModels
 {
     public class SeasonsViewModel : Screen
     {
+        private BindingList<string> _seasons;
+        private string _seasonFullName;
+        private string _seasonShortName;
+
+        public BindingList<string> Seasons
+        {
+            get { return _seasons; }
+            set 
+            { 
+                _seasons = value;
+                NotifyOfPropertyChange(() => Seasons);
+            }
+        }
+
+        public string SeasonFullName
+        {
+            get { return _seasonFullName; }
+            set 
+            { 
+                _seasonFullName = value;
+                NotifyOfPropertyChange(() => SeasonFullName);
+            }
+        }
+
+        public string SeasonShortName
+        {
+            get { return _seasonShortName; }
+            set 
+            { 
+                _seasonShortName = value;
+                NotifyOfPropertyChange(() => SeasonShortName);
+            }
+        }
+
+        public bool CanSetAsCurrent {
+            get 
+            {
+                bool output = false;
+
+                //TODO : Inserire il codice per capire se una riga della DataGrid "SeasonsList" è stata selezionata
+                output = true;
+
+                return output;
+            }
+        }
+
+        public void SetAsCurrent() 
+        {
+
+        }
     }
 }
